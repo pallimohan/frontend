6c403f394b76d6cee7314b091dc9d246a6032ac2
pallimohan <pallimohan8008@gmail.com>
2025-08-26 15:21:25 +0530

    save

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  build: {
+    outDir: "dist",
+  },
+  base: "/", // important for Vercel routing
+});
